c3f754b81075596255c6c750d2142c9bbb284e70
herreang000 <herreang000@warren.k12.in.us>
2025-02-07 08:12:52 -0500

    final

---
+++ script.js
@@ -0,0 +1,23 @@
+class ValidationError extends Error{
+    constructor(message){
+        super(message)
+        this.ValidationError = new ValidationError();
+    }
+}
+
+function validateUsername(username) {
+    if(username.length < 10){
+        throw new ValidationError("The username is less than 10 characters")
+    }
+    else{
+        return "Username is Valid";
+    }
+}
+
+
+
+try{
+    console.log(validateUsername("abc"));
+}catch(error){
+    console.error(`${error.name}: ${error.message}`)
+}
